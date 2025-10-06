14f0ca24205c9c81275d1c42a2f2c0fb420eab48
angel.sanchez <angel.sanchez@sedecal.com>
2025-10-06 08:44:51 +0200

    continuacion formularios reactivos

--- 06-reactive-forms-app/src/app/app.routes.ts
+++ 06-reactive-forms-app/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'country',
+    loadChildren: () => import('./country/country.routes').then(mod => mod.countryRoutes)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes') //NO HACE FALTA THEN PORQUE LO HEMOS EXPORTADO POR DEFECTO
+  },
+  {
+    path: 'reactive',
+    loadChildren: () => import('./reactive/reactive.routes').then(mod => mod.reactiveRoutes)
+  },
+  {
+    path: '**',
+    redirectTo: 'reactive'
+  }
+];
--- 06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,11 +1,17 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.component.html',
   styleUrl: './basic-page.component.css'
 })
 export class BasicPageComponent {
-
+   myForm = new FormGroup({
+     name: new FormControl(''),
+     price: new FormControl(0),
+     inStorage: new FormControl(0),
+   });
 }
